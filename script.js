05677ebebcfbd0bc71529b9964a17aed6426b2ee
FoxDev <91700492+ItsFoxDev@users.noreply.github.com>
2023-02-20 18:43:18 -0500

    🚧 • Moved some functions here (with doc)

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-function keepState(elem)
-{
+function keepState(elem){
   var key = "save-" + elem.id;
-
   var storedValue = localStorage.getItem(key);
-
   if (storedValue)
       elem.value = storedValue;
-
   elem.addEventListener('input', function (){
       cLog('Attempting to save to local storage','darkorange');
       localStorage.setItem(key, elem.value);
@@ -17,7 +13,6 @@ function keepState(elem)
 function cLog(m,c){
   console.log("%cFoxJS","color: white; background: " + c + "; padding: 2px 6px; border-radius: 3px; margin-right: 5px;",m);
 }
-
 
 const saves = document.querySelectorAll('.save');
 Array.from(saves).forEach((element, index) => {
@@ -40,4 +35,31 @@ function wordCount() {
   'info'
 )
 cLog('Counted ' + wcount + 'words','darkgreen');
+}
+function clearZone(){
+Swal.fire({
+  title: 'Clear PasteZone?',
+  text: 'You won\'t be able to revert this!',
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#2eb00b',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Yes, clear it!'
+}).then((result) => {
+  if (result.isConfirmed) {
+      document.getElementById('area').value='';
+    Swal.fire(
+      'Success!',
+      'Your PasteZone has been cleared.',
+      'success'
+    )
+  }
+})
+};
+
+function copyZText(){
+  /* Selects text */ document.querySelector('textarea').select();
+  /* Copies text */ document.execCommand('copy');
+  /* De-selects text */ window.getSelection().removeAllRanges();
+  Swal.fire('Success!','Pastezone has been copied to clipboard.','success')
 }
